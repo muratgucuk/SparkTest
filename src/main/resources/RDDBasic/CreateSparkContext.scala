cd3da502dd4e0a9a73c87859c3360f638a85922e
muratgucuk <muratgucuk54@gmail.com>
2019-04-06 12:56:18 +0300

    word count streaming example created

---
+++ src/main/resources/RDDBasic/CreateSparkContext.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+object CreateSparkContext {
+  
+  def main(args: Array[String]) {
+    val logFile = "C:\\spark\\README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("CreateSparkContext").config("spark.master", "local").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+    
+  }
+}
